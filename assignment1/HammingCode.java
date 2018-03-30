6130ea75edd2ef978f80a31b4dd11f5980428cff
Emanuela <dookie@live.it>
2018-03-30 22:40:57 +0200

    fixed bugs, new methods, working on decode

--- assignment1/HammingCode.java
+++ assignment1/HammingCode.java
@@ -8,92 +8,140 @@ import java.nio.file.Paths;
 public class HammingCode {
 	
 	//generating matrix
-	public static final boolean[][] G = {
-			{true,	true,	false,	true},		// [ 1, 1, 0, 1 ]
-			{true,	false,	true,	true},		// [ 1, 0, 1, 1 ]
-			{true,	false,	false,	false},		// [ 1, 0, 0, 0 ]
-			{false,	true,	true,	true},		// [ 0, 1, 1, 1 ]
-			{false,	true,	false,	false},		// [ 0, 1, 0, 0 ]
-			{false,	false,	true,	false},		// [ 0, 0, 1, 0 ]
-			{false,	false,	false,	true}		// [ 0, 0, 0, 1 ]
+	public static final boolean[][] G = {		//data  d1 d2 d3 d4
+			{true,	true,	false,	true},		// p1 [ 1, 1, 0, 1 ]
+			{true,	false,	true,	true},		// p2 [ 1, 0, 1, 1 ]
+			{true,	false,	false,	false},		// d1 [ 1, 0, 0, 0 ]
+			{false,	true,	true,	true},		// p3 [ 0, 1, 1, 1 ]
+			{false,	true,	false,	false},		// d2 [ 0, 1, 0, 0 ]
+			{false,	false,	true,	false},		// d3 [ 0, 0, 1, 0 ]
+			{false,	false,	false,	true}		// d4 [ 0, 0, 0, 1 ]
 		};
 	
 	//parity-check matrix
-	public static final boolean[][] H = {
-			{true,	false,	true,	false,	true,	false,	true},		// [ 1, 0, 1, 0, 1, 0, 1 ]
-			{false,	true,	true,	false,	false,	true,	true},		// [ 0, 1, 1, 0, 0, 1, 1 ]
-			{false,	false,	false,	true,	true,	true,	true}		// [ 0, 0, 0, 1, 1, 1, 1 ]
+	public static final boolean[][] H = {								//index 1  2  3  4  5  6  7
+			{true,	false,	true,	false,	true,	false,	true},		// p1 [ 1, 0, 1, 0, 1, 0, 1 ]
+			{false,	true,	true,	false,	false,	true,	true},		// p2 [ 0, 1, 1, 0, 0, 1, 1 ]
+			{false,	false,	false,	true,	true,	true,	true}		// p3 [ 0, 0, 0, 1, 1, 1, 1 ]
 		};
 	
-	//dot product
-	public static boolean multiplyVector(boolean[] v1, boolean[] v2) { 
-		if (v1.length != v2.length)
+	// dot product
+	public static boolean multiplyVector(boolean[] v1, boolean[] v2) {
+		//handle exceptions
+		if (v1.length != v2.length) { // TODO throw new Exception(); maybe throw exception if different lengths
 			System.out.println("Different lengths: " + v1.length + ", " + v2.length);
-		boolean[] temp = new boolean[v1.length];
+		}
+		
 		boolean res = false;
-		int i;
-		
-		//from 0 to 7
-		for (i=0; i<v1.length; i++) {
-			temp[i] = v1[i] && v2[i];
-			System.out.print("" + i + temp[i] + ", ");
+		for (int i = 0; i < v1.length; i++) {
+			res = res != (v1[i] && v2[i]);
 		}
-		
-		//from 0 to 7
-		for(i=0; i < temp.length; i++) {
-			res = res != temp[i];
-		}
-		System.out.println(res);
 		return res;
 	}
-	
-	//parity check
-	public static boolean[] multiplyMatrix(boolean[][] m, boolean[] v){
+
+	// parity check
+	public static boolean[] multiplyMatrix(boolean[][] m, boolean[] v) {
 		boolean[] res = new boolean[m.length];
-		
-		//from 0 to 2
-		for (int i=0; i<m.length; i++) {
+
+		for (int i = 0; i < m.length; i++) {
 			res[i] = multiplyVector(m[i], v);
+			if (res[i])
+				System.out.println(i);
 		}
-		System.out.println(res[0]+ ", "+ res[1] + ", "+res[2]);
 		return res;
 	}
-	
-	private static void writeToFile(boolean[] v, String filename) {
-		// TODO transform boolean vector into string of 0s and 1s and print it into the file
+
+	public static boolean[] stringToBooleanVector(String s) {
+		boolean[] res = new boolean[s.length()];
+
+		for (int i = 0; i < s.length(); i++) {
+			switch (s.charAt(i)) {
+			case '0':
+				res[i] = false;
+				break;
+			case '1':
+				res[i] = true;
+				break;
+			default:
+				// TODO throw new Exception(); throw exception if not 0 or 1 + print error message
+				break; 
+			}
+		}
+
+		return res;
 	}
-	
-	private static boolean[] readFromFile(String filename) {
+
+	public static String booleanVectorToString(boolean[] v) {
+		char[] res = new char[v.length];
+		for (int i = 0; i < v.length; i++) {
+			res[i] = v[i] ? '1' : '0';
+		}
+		return new String(res);
+	}
+
+	public static boolean isAll0(boolean[] v) {
+		for (int i = 0; i < v.length; i++)
+			if (v[i])
+				return false;
+		return true;
+	}
+
+	public static void writeToFile(boolean[] v, String filename) {
+		// TODO transform boolean vector into string of 0s and 1s and print it into the
+		// file
+	}
+
+	public static boolean[] readFromFile(String filename) {
 		// TODO read the given file and transform it into a boolean vector
-		boolean[] v = {true, false};	//dummy vector
+		boolean[] v = { true, false }; // dummy vector
 		return v;
 	}
-	
+
+	private static String encodeBits(String data) {
+		// TODO maybe use vector-matrix multiplication for more efficiency
+
+		int d1 = Integer.parseInt(data.charAt(0) + "");
+		int d2 = Integer.parseInt(data.charAt(1) + "");
+		int d3 = Integer.parseInt(data.charAt(2) + "");
+		int d4 = Integer.parseInt(data.charAt(3) + "");
+
+		int p1 = (d1 + d2 + d4) % 2;
+		int p2 = (d1 + d3 + d4) % 2;
+		int p3 = (d2 + d3 + d4) % 2;
+
+		return "" + p1 + p2 + d1 + p3 + d2 + d3 + d4;
+	}
+
 	public static void encode(String message, String filename) {
+		// TODO maybe use char[] for more efficiency
+
 		String result = "";
-		for(int i = 0; i<message.length(); i++) {
-			//read ith character, convert it to 8-character-long binary string, add leading 0s if shorter
+		for (int i = 0; i < message.length(); i++) {
+			// read ith character, convert it to 8-character-long binary string, add leading
+			// 0s if shorter
 			String temp = String.format("%8s", Integer.toBinaryString(message.charAt(i))).replace(' ', '0');
-			result += encodeBits(temp.substring(0,4)) + encodeBits(temp.substring(4,8));
-			}
-		
+			result += encodeBits(temp.substring(0, 4)) + encodeBits(temp.substring(4, 8));
+		}
+		System.out.println(result);
+
 		try (PrintStream out = new PrintStream(new FileOutputStream(filename))) {
-		    out.print(result);
+			out.print(result);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String decode(String filename) {
-        String content = "";
-        String result = "";
+		String content = "";
+		String result = "";
 		try {
-			//read input file in one step and store the content into a string, without having to iterate line by line
-			//a valid input file will contain only one line of code anyway
+			// read input file in one step, store the content into a string, without having
+			// to iterate line by line
+			// a valid input file will contain only one line of code anyway
 			content = new String(Files.readAllBytes(Paths.get(filename)));
 			if (content.length() % 7 != 0) {
-				//throw exception: se la lunghezza della stringa non è multipla di 7
+				// throw exception: se la lunghezza della stringa non è multipla di 7
 				throw new Exception();
 			}
 		} catch (IOException e) {
@@ -103,99 +151,69 @@ public class HammingCode {
 			System.err.println("Input length " + content.length() + " not valid: must be multiple of 7");
 			e.printStackTrace();
 		}
-		
-		String temp = "";
-		
-		//finché la stringa content non è vuota:
-		//	temp = leggi i primi 7 caratteri di content,
-		//	fai cose (decodifica, correzione degli errori, concateni il risultato a result)
-		//	modifichi content: content MENO i primi 7 caratteri
+
+		//while string content not empty:
+		// temp = read first 7 characters of content
+		// do stuff: decode, correct errors if needed, concatenate correct result to res
+		// reassignment: content = content MINUS first 7 characters
+
 		while (content.length() != 0) {
-			boolean[] tempArray = new boolean[6];
-			temp = content.substring(0,7);
-			// TODO do stuff with temp: check for errors and decode
+			// trasformo temp in array di boolean <- created another method for this
 
-			//trasformo temp in array di boolean
-			for(int i = 0; i<temp.length(); i++){
-				if(temp.charAt(i) == '0'){
-					tempArray[i] = false;
-				}
-				if(temp.charAt(i) == '1'){
-					tempArray[i] = true;
-				}
-			}
+			boolean[] temp = stringToBooleanVector(content.substring(0, 7));
 
-			//parity check
-			boolean[] checkedVector = multiplyMatrix(H,tempArray);
-			
-			//all 0?
-			boolean all0 = true;
-			for(int j = 0; j<checkedVector.length; j++){
-				if(checkedVector[j]){
-					all0 = false;
-				}
-			}
-			
+			// parity check
+			boolean[] checkedVector = multiplyMatrix(H, temp);
+
+			// all 0? <- created another method for this
+			boolean all0 = isAll0(checkedVector);
+
 			String correctStr = "";
 
-			//error correction
-			if(all0 == false){
-				boolean[] correctVector = errorCorrector(tempArray);
-					for(int k = 0; k<correctVector.length; k++){
-						if(correctVector[k] == true){
-							correctStr += 1;
-						}
-						if(correctVector[k] == false){
-							correctStr += 0;
-						}
-					}
-					result += correctStr;
-			}else{
-				result += temp;
+			// error correction
+			if (all0) {
+				result += booleanVectorToString(temp);
+				continue;
 			}
-			//concatenate correctStr to result string
-			content = content.substring(7);
+
+			boolean[] correctVector = errorCorrector(temp);
+			
+			for (int k = 0; k < correctVector.length; k++) {
+				if (correctVector[k] == true) {
+					correctStr += 1;
+				}
+				if (correctVector[k] == false) {
+					correctStr += 0;
+				}
+				// concatenate correctStr to result string
+				result += correctStr;
+
+				//content = content MINUS first 7 characters
+				content = content.substring(7);
+			}
 		}
-		
 		return result;
 	}
-	
-	private static String encodeBits(String data) {
-		int p1, p2, p3;
-		int d1, d2, d3, d4;
-		
-		d1 = Integer.parseInt(data.charAt(0)+"");
-		d2 = Integer.parseInt(data.charAt(1)+"");
-		d3 = Integer.parseInt(data.charAt(2)+"");
-		d4 = Integer.parseInt(data.charAt(3)+"");
-		
-		p1 = (d1 + d2 + d4)%2;
-		p2 = (d1 + d3 + d4)%2;
-		p3 = (d2 + d3 + d4)%2;
-		
-		return ""+p1+p2+d1+p3+d2+d3+d4;
-	}
-	
+
 	private static boolean[] errorCorrector(boolean[] vector) {
-		
+
 		boolean[] toBeCorrected = multiplyMatrix(H, vector);
 
-		//transform boolean values in 0s and 1s (binary string) 
-		//and calculate the equivalent integer
+		// transform boolean values in 0s and 1s (binary string) <- created new method for this
+		// and calculate the equivalent integer
 		String temp = "";
-		for(int g = 0; g<toBeCorrected.length; g++){
+		for (int g = 0; g < toBeCorrected.length; g++) {
 			temp = toBeCorrected[g] + temp;
 		}
-		
-		int numOfColumn = Integer.parseInt(temp,2);
 
-		for(int h = 0; h<vector.length; h++){
-			if(h == numOfColumn){
-				vector[h] = !vector[h]; 
+		int numOfColumn = Integer.parseInt(temp, 2);
+
+		for (int h = 0; h < vector.length; h++) {
+			if (h == numOfColumn) {
+				vector[h] = !vector[h];
 			}
 		}
 
 		return vector;
 	}
 }
-
--- assignment1/Main.java
+++ assignment1/Main.java
@@ -4,20 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		String file = "result.txt";
 		
-		boolean[] b1 = {false, true, true, false, false, true, true};
-		boolean[] b2 = {true, true, true, false, false, true, true};
+//		boolean[] b1 = {false, true, true, false, false, true, true};
+//		boolean[] b2 = {true, true, true, false, false, true, true};
+//		boolean[] b3 = {false, true, true, false, true, true, true};
+//		HammingCode.multiplyVector(b1, b1);
+//		HammingCode.multiplyVector(b1, b2);
+//		System.out.println();
+//		HammingCode.multiplyMatrix(HammingCode.H, b1);
+//		System.out.println();
+//		HammingCode.multiplyMatrix(HammingCode.H, b2);
+//		System.out.println();
+//		HammingCode.multiplyMatrix(HammingCode.H, b3);
 		
-		HammingCode.multiplyVector(b1, b1);
-		HammingCode.multiplyVector(b1, b2);
-		
-		System.out.println();
-		HammingCode.multiplyMatrix(HammingCode.H, b1);
-		
-		System.out.println();
-		HammingCode.multiplyMatrix(HammingCode.H, b2);
-		
-//		HammingCode.encode("blablaciao", file);
-//		System.out.println(HammingCode.decode(file));
+		HammingCode.encode("blablaciao", file);
+		System.out.println(HammingCode.decode(file));
 	}
 
 }
