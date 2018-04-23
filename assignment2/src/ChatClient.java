f6d3e27bd5d245776b3c3472f376fd7ec93c4208
Emanuela <dookie@live.it>
2018-04-23 22:05:32 +0200

    written client but still to test

--- assignment2/src/ChatClient.java
+++ assignment2/src/ChatClient.java
@@ -1,22 +1,73 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class ChatClient {
+public class ChatClient extends Thread {
+	Socket socket;
+	String username, buffer;
+	DataInputStream in;
+	DataOutputStream out;
+	boolean connection;
 	
-	// some instance variables from the tutorial
-	// still have to figure out whether we actually need them lol
+	public ChatClient() {
+	}
 	
-	Socket clientSocket;
-	DataInputStream inputStream;
-	PrintStream outputStream;
+	public ChatClient(String username, String host, int port) {
+		connection = false;
+		this.username = username;
 
-	public static void main(String args[]) throws IOException {
-		
-		// ask client to enter username, host address and port ???
+		try {
+			socket = new Socket(InetAddress.getByName(host), port);
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			connection = true;
+			
+			new ChatClient().start();
+			while(connection) {
+				out.writeUTF(username + ": " + buffer);
+			}
+
+			in.close();
+			out.close();
+			socket.close();
+		} catch (UnknownHostException e) {
+			connection = false;
+			System.err.println("Unknown host" + host + "\nTerminating");
+			e.printStackTrace();
+		} catch (IOException e) {
+			connection = false;
+			System.err.println("Connection error\nTerminating");
+			e.printStackTrace();
+		}
+
+		run();
+	}
+
+	@Override
+	public void run() {
+		Scanner scanner = new Scanner(System.in);
+		while (connection) {
+			buffer = scanner.nextLine();
+			try {
+				out.writeUTF(buffer);
+				if (buffer.equals("/quit")) {
+					connection = false;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		scanner.close();
+	}
+
+	// remove throws clause and handle exceptions internally
+	public static void main(String args[]) {
+
 		// if successful start connection (open socket)
 		// else print error message and quit
 		// once connected loop for user input
@@ -24,16 +75,19 @@ public class ChatClient {
 		// disconnect when userInput.equals("/quit");
 		// ask for confirmation do you want to disconnect? yes/no
 		// close socket
-		
-		int serverPort = 6789;
-		Socket s = new Socket(InetAddress.getLocalHost(), serverPort);
-		DataInputStream in = new DataInputStream(s.getInputStream());
-		DataOutputStream out = new DataOutputStream(s.getOutputStream());
-		for (int i = 0; i < 5; i++) {
-			out.writeUTF("Teststring"); // UTF is a string encoding
-			String data = in.readUTF(); // a blocking call
-			System.out.println("Got back: \"" + data + "\"");
-		}
-		s.close();
+		// if connected print connection successful
+		// else print error message couldn't connect
+
+		// int serverPort = 6789;
+		// Socket s = new Socket(InetAddress.getLocalHost(), serverPort);
+		// DataInputStream in = new DataInputStream(s.getInputStream());
+		// DataOutputStream out = new DataOutputStream(s.getOutputStream());
+		// for (int i = 0; i < 5; i++) {
+		// out.writeUTF("Teststring"); // UTF is a string encoding
+		// String data = in.readUTF(); // a blocking call
+		// System.out.println("Got back: \"" + data + "\"");
+		// }
+		// s.close();
 	}
+
 }
---
+++ assignment2/src/ClientMain.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// ask user for username, host address and port
+		System.out.print("Type your username: ");
+		String username = scanner.nextLine();
+		System.out.print("Type server address: ");
+		String host = scanner.nextLine();
+		System.out.print("Type port number: ");
+		int port = scanner.nextInt();
+
+		scanner.close();		
+		new ChatClient(username, host, port);
+		
+	}
+
+}
